3672d53b46cd52cee859a0cb7646c3623ce82a36
arthur_rabell <arthur.rabel@outlook.com>
2024-06-12 11:01:49 -0300

    fix: createFeaturePrevision.js create feature polygon

--- frontEnd/src/openLayers/createFeaturePrevision.js
+++ frontEnd/src/openLayers/createFeaturePrevision.js
@@ -1,10 +1,23 @@
 import Feature from 'ol/Feature';
 import Point from 'ol/geom/Point';
+import LineString from 'ol/geom/LineString';
+import Polygon from 'ol/geom/Polygon';
 
-const createFeaturePrevision = (evento) => {
+const createFeaturePrevision = (coordinate) => {
+    let arrayCoordinate = [...coordinate];
+    let geometryType;
+    if(arrayCoordinate.length == 1) {
+        geometryType = new Point(arrayCoordinate[0]);
+    } else if(arrayCoordinate.length == 2) {
+        geometryType = new LineString(arrayCoordinate);
+    } else {
+        arrayCoordinate.push(arrayCoordinate[0])
+        geometryType = new Polygon([arrayCoordinate]);
+    }
+
     return new Feature({
-        geometry: new Point(evento.coordinate),
-    })
+        geometry: geometryType,
+    });
 }
 
 export default createFeaturePrevision;
