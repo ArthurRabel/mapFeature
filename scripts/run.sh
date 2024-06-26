c091b2978ae167df8c0cdb6214bfd8ed8823aad5
ArthurRabel <arthur.rabel@outlook.com>
2024-06-25 22:01:14 -0300

    chore: organize shell scripts into a new directory

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+podmanversion="$(podman --version)"
+
+if (( $(echo "${podmanversion:15:3} < 4.9" | bc -l) )); then
+    echo -e "Vixe! A versão do podman está desatualizado! \nAcesse a documentação do podman para baixar uma versão superior a 4.9"
+    exit 1 
+fi
+
 ## Verificando se o pod ja existe
-echo "Verificando se o pod ja existe..."
-printf "\n"
+echo -e "Inicializando o projeto...\n"
+
 podman pod exists map-feature-latest > /dev/null
 if [ $? -eq 0 ]; then
     echo "Pod ja existe"    
@@ -10,15 +17,10 @@ if [ $? -eq 0 ]; then
     echo "Iniciando Pod..."
     podman pod start map-feature-latest > /dev/null
 
-    bash sucessoEcho.sh
+    bash scripts/sucessoEcho.sh
 else
     echo "Projeto está sendo executado pela primeira vez."
-    echo "Preparando os requisitos..."
-
-    ## Create secrets
-    echo "Criando secrets..."
-    podman secret create adminCredential adminSecret.yaml > /dev/null
-    podman secret create apiCredential fastApiSecret.yaml > /dev/null
+    echo -e "Preparando o necessario para rodar... \n"
 
     ## Create build
     echo "Criando build..."
